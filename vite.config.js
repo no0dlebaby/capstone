36eddecb38e70e8a2a3c2559145aff4b178d9fb7
sophia <167137213+no0dlebaby@users.noreply.github.com>
2024-10-11 00:15:23 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: './frontend',
   plugins: [react()],
   build: {
-    outDir: '../dist',
+    outDir: '../dist', 
     emptyOutDir: true,
   },
 });
