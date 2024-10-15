00864b7dca99c2331b2e2b53c47e98a7af72e844
sophia <167137213+no0dlebaby@users.noreply.github.com>
2024-10-14 20:23:09 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   root: 'frontend',
   plugins: [react()],
   build: {
-    outDir: '../dist', 
-    emptyOutDir: true,
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
   },
 });
